a5dce85167da6b8df5fb2168dd84f92896c2e1b4
GordionA1 <farisazka2506@gmail.com>
2026-03-10 22:08:52 +0700

    membuat fungsi pengkategorian bmi

--- PerhitunganBMI.cpp
+++ PerhitunganBMI.cpp
@@ -9,3 +9,22 @@ float NilBB, NilTB ;
 float perhitungan_bmi(float a,float b){
     return a/(b*b);
 }
+
+// membuat fungsi pengkategorian bmi
+string bmi (float r){
+    if (r < 18.5)
+        return "berat badan kurang";
+    else{
+        if (r <= 25.00){
+            return "berat badan normal";
+        }
+        else {
+            if (r < 30){
+                return "kelebihan berat badan";
+            }
+            else {
+                return "obesitas";
+            }
+        }
+    }
+}
